b2cbfde1b2ff9eea917c7dfc93c5eb3965a4a0be
MacKenzie Richards <81762014+mrichards03@users.noreply.github.com>
2024-11-22 22:04:31 -0800

    1.6-1.8 written parts done

--- QRAlgorithm(1.6-1.8).py
+++ QRAlgorithm(1.6-1.8).py
@@ -28,5 +28,7 @@ print(f"Eigenvalues of A5 from QR:\n{eigenvalues}\n")
 #1.8
 B = A5 + 0.9*np.identity(2)
 print(f"Original matrix B:\n{B}\n")
+eigenvalues = ", ".join(map(str, (QRAlgorithm(B))))
+print(f"Eigenvalues of B from QR:\n{eigenvalues}\n")
 eigenvalues = ", ".join(map(str, (QRAlgorithm(B) - 0.9).round(2)))
 print(f"Eigenvalues of A5 from QR shifted:\n{eigenvalues}\n")
